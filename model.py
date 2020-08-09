a71762ade0e1f13d59cd7d49729e11a8d8b63c3d
Dishon Merkhai <dmerkhai@uwo.ca>
2020-08-09 01:20:45 -0400

    Add the U-Net model

---
+++ model.py
@@ -0,0 +1,140 @@
+import numpy as np 
+import os
+import skimage.io as io
+import skimage.transform as trans
+import numpy as np
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as K
+
+def jaccard_distance(y_true, y_pred, smooth=100):
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return (1 - jac) * smooth
+
+# TODO: Fix the dice loss function
+def dice_coef(y_true, y_pred):
+    smooth = 1
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection +smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) +smooth)
+
+def dice_coef_loss(y_true, y_pred):
+    print("dice loss")
+    return 1-dice_coef(y_true, y_pred)
+
+def specificity(y_true,y_pred):
+    specificity=0
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    TP=K.sum(y_true_f*y_pred_f)
+    TN=K.sum((1-y_true_f)*(1-y_pred_f))
+    FP=K.sum((1-y_true_f)*y_pred_f)
+    FN=K.sum(y_true_f*(1-y_pred_f))
+    specificity=(TN)/((TN+FP))
+    return specificity
+
+def sensitivity(y_true,y_pred):
+    sensitivity=0
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    TP=K.sum(y_true_f*y_pred_f)
+    TN=K.sum((1-y_true_f)*(1-y_pred_f))
+    FP=K.sum((1-y_true_f)*y_pred_f)
+    FN=K.sum(y_true_f*(1-y_pred_f))
+    sensitivity=(TP)/((TP+FN))
+       
+    return sensitivity
+
+
+def unet(pretrained_weights = None,input_size = (256,256,1)):
+    inputs = Input(input_size)
+    conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+    # print(conv1)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+    conv1 = BatchNormalization()(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+    conv2 = BatchNormalization()(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+    conv4 = BatchNormalization()(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+    conv5 = BatchNormalization()(conv5)
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+    conv5 = BatchNormalization()(conv5)
+    drop5 = Dropout(0.5)(conv5)
+
+    up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(drop5))
+    up6 = BatchNormalization()(up6)
+    merge6 = concatenate([drop4, up6], axis=3)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
+    conv6 = BatchNormalization()(conv6)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
+    conv6 = BatchNormalization()(conv6)
+    up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+        UpSampling2D(size=(2, 2))(conv6))
+    up7 = BatchNormalization()(up7)
+    merge7 = concatenate([conv3, up7], axis=3)
+
+    conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
+    conv7 = BatchNormalization()(conv7)
+
+    conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
+
+    conv7 = BatchNormalization()(conv7)
+    up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+        UpSampling2D(size=(2, 2))(conv7))
+    up8 = BatchNormalization()(up8)
+    merge8 = concatenate([conv2, up8], axis=3)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
+    conv8 = BatchNormalization()(conv8)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
+    conv8 = BatchNormalization()(conv8)
+    up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+        UpSampling2D(size=(2, 2))(conv8))
+    up9 = BatchNormalization()(up9)
+    merge9 = concatenate([conv1, up9], axis=3)
+
+    conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
+    conv9 = BatchNormalization()(conv9)
+
+    conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+    conv9 = BatchNormalization()(conv9)
+
+    conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+    conv9 = BatchNormalization()(conv9)
+
+    conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
+    
+
+    model = Model(inputs, conv10)
+
+    model.compile(optimizer = Adam(lr = 1e-4), loss = jaccard_distance, metrics = ['accuracy'])
+    
+    #model.summary()
+
+    if(pretrained_weights):
+      model.load_weights(pretrained_weights)
+
+    return model
